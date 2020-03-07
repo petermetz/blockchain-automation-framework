2e4f6f0d5d812958f4e393344c4d0b1b9480c169
petermetz <peter.metz@unarin.com>
2020-03-07 05:27:09 +0000

    Pushing deployment files for namespace, service accounts and clusterrolebinding

---
+++ setup-ubuntu-bionic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x
+# microk8s.helm delete --purge flux-local
+
+rm -rf ../vault/*
+
+export VAULT_ADDR='http://10.0.0.2:8200' ;
+
+vault server -config=../config.hcl -log-level=debug &
+VAULT_PID=$!
+echo "VAULT PID: ${VAULT_PID}"
+sleep 2
+vault operator init -key-shares=1 -key-threshold=1 -format=json > vault-init-output.json
+
+VAULT_ROOT_TOKEN="$(cat vault-init-output.json | jq .root_token --raw-output)"
+echo "VAULT_ROOT_TOKEN => $VAULT_ROOT_TOKEN"
+sed -i "s/root_token: \".*\"/root_token: \"$VAULT_ROOT_TOKEN\"/g" build/network.yaml
+
+VAULT_UNSEAL_KEY_B64="$(cat vault-init-output.json | jq .unseal_keys_b64[0] --raw-output)"
+echo "VAULT_UNSEAL_KEY_B64 => $VAULT_UNSEAL_KEY_B64"
+
+export VAULT_TOKEN=$VAULT_ROOT_TOKEN;
+vault operator unseal $VAULT_UNSEAL_KEY_B64
+vault secrets enable -version=1 -path=secret kv
+
+
+microk8s.reset --destroy-storage
+microk8s.status --wait-ready
+microk8s.enable rbac dns storage helm
+microk8s.status --wait-ready
+
+microk8s.kubectl delete clusterrole flux-local
+
+docker run -v $(pwd):/home/blockchain-automation-framework/ hyperledgerlabs/baf-build || kill $VAULT_PID
