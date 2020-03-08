338b366e54154485b869beb2974755678771b7d7
petermetz <peter.metz@unarin.com>
2020-03-08 17:05:00 +0000

    Pushing deployment files for namespace, service accounts and clusterrolebinding

--- setup-ubuntu-bionic.sh
+++ setup-ubuntu-bionic.sh
@@ -11,19 +11,18 @@ vault server -config=../config.hcl -log-level=debug &
 VAULT_PID=$!
 echo "VAULT PID: ${VAULT_PID}"
 sleep 2
-vault operator init -key-shares=1 -key-threshold=1 -format=json > vault-init-output.json
+vault operator init -key-shares=1 -key-threshold=1 -format=json > ./build/vault-init-output.json
 
-VAULT_ROOT_TOKEN="$(cat vault-init-output.json | jq .root_token --raw-output)"
+VAULT_ROOT_TOKEN="$(cat ./build/vault-init-output.json | jq .root_token --raw-output)"
 echo "VAULT_ROOT_TOKEN => $VAULT_ROOT_TOKEN"
 sed -i "s/root_token: \".*\"/root_token: \"$VAULT_ROOT_TOKEN\"/g" build/network.yaml
 
-VAULT_UNSEAL_KEY_B64="$(cat vault-init-output.json | jq .unseal_keys_b64[0] --raw-output)"
+VAULT_UNSEAL_KEY_B64="$(cat ./build/vault-init-output.json | jq .unseal_keys_b64[0] --raw-output)"
 echo "VAULT_UNSEAL_KEY_B64 => $VAULT_UNSEAL_KEY_B64"
 
 export VAULT_TOKEN=$VAULT_ROOT_TOKEN;
 vault operator unseal $VAULT_UNSEAL_KEY_B64
 vault secrets enable -version=1 -path=secret kv
-
 
 microk8s.reset --destroy-storage
 microk8s.status --wait-ready
